0767d614babd73a1c7e73b7372292d324e585939
Dane Wommack <dwommack@oreillyauto.com>
2017-02-22 09:57:13 -0600

    Cleaned up spacing a bit

--- core/src/main/java/com/dwommack/PressTheRedButton/core/PressTheRedButton.java
+++ core/src/main/java/com/dwommack/PressTheRedButton/core/PressTheRedButton.java
@@ -65,7 +65,6 @@ public class PressTheRedButton extends SceneGame {
   //A method to paint the screen with different layers when needed  
   public void populateScreen(ImageLayer bgLayer, GroupLayer layer){
     ImageLayer gameOverLayer = new ImageLayer(gameOverImage);
-	
     rootLayer.removeAll();
     bgLayer.setSize(plat.graphics().viewSize);
 	
@@ -75,58 +74,57 @@ public class PressTheRedButton extends SceneGame {
       rootLayer.add(layer);
     }
 	
-	  //You're playing the game - paints playing the game images
-	  if (gameState == PLAYING){
-      //resets these values when the player begins a new game
+	//You're playing the game - paints playing the game images
+	if (gameState == PLAYING){
+    //resets these values when the player begins a new game
       score = 0;
       strikeCounter = 0;
       gameCounter = 0;
       gameCounterDifficulty = 0;
-		  rootLayer.add(bgLayer);
-		  rootLayer.add(layer);
-	  }
-	  //Player lost the game by either pressing a non-red button or missing a total of 3 presses in their game
-	  if (gameState == GAMEOVER){
+	  rootLayer.add(bgLayer);
+	  rootLayer.add(layer);
+	}
+	//Player lost the game by either pressing a non-red button or missing a total of 3 presses in their game
+	if (gameState == GAMEOVER){
       rootLayer.add(layer);
       rootLayer.addFloorAt(gameOverLayer,50, 10);
-	  }
+	}
   }
 	
-	public void updateScore(StringBuilder scoreDisplay, CanvasLayer scoreBoardLayer, Canvas canvas){
-    scoreDisplay.delete(0,scoreDisplay.length());
-    scoreDisplay.append("Score \n"+ score);
-    scoreBoardLayer.begin();
-    TextBlock block = new TextBlock(plat.graphics().layoutText(
-	     			scoreDisplay.toString(), 
-						new TextFormat(new Font("Helvetica", Font.Style.BOLD, 20)),
-						TextWrap.MANUAL));
-    canvas.clear();
-    canvas.setFillColor(0xFFFFFFFF);
-    block.fill(canvas, TextBlock.Align.LEFT, 2, 2);
-            rootLayer.addFloorAt(scoreBoardLayer, 20, 20);  
-            scoreBoardLayer.end();
-		}
-
-	public void updateStrikeCounter(StringBuilder strikeCounterDisplay, CanvasLayer strikeLayer, Canvas canvas2){
-    strikeCounterDisplay.delete(0, strikeCounterDisplay.length());
-    strikeCounterDisplay.append("Strike Counter: \n" + strikeCounter);
-    strikeLayer.begin();
-    TextBlock block2 = new TextBlock(plat.graphics().layoutText(
-						strikeCounterDisplay.toString(), 
-						new TextFormat(new Font("Helvetica", Font.Style.BOLD, 20)),
-						TextWrap.MANUAL));
-    canvas2.clear();
-    canvas2.setFillColor(0xFFFFFFFF);
-    block2.fill(canvas2, TextBlock.Align.LEFT, 2, 2);
-    rootLayer.addFloorAt(strikeLayer, 600, 20);
-    strikeLayer.end();	
+  public void updateScore(StringBuilder scoreDisplay, CanvasLayer scoreBoardLayer, Canvas canvas){
+	  scoreDisplay.delete(0,scoreDisplay.length());
+	  scoreDisplay.append("Score \n"+ score);
+	  scoreBoardLayer.begin();
+	  TextBlock block = new TextBlock(plat.graphics().layoutText(
+	   				scoreDisplay.toString(), 
+					new TextFormat(new Font("Helvetica", Font.Style.BOLD, 20)),
+					TextWrap.MANUAL));
+	  canvas.clear();
+	  canvas.setFillColor(0xFFFFFFFF);
+	  block.fill(canvas, TextBlock.Align.LEFT, 2, 2);
+	  rootLayer.addFloorAt(scoreBoardLayer, 20, 20);  
+	  scoreBoardLayer.end();
 	}
+
+  public void updateStrikeCounter(StringBuilder strikeCounterDisplay, CanvasLayer strikeLayer, Canvas canvas2){
+	  strikeCounterDisplay.delete(0, strikeCounterDisplay.length());
+	  strikeCounterDisplay.append("Strike Counter: \n" + strikeCounter);
+	  strikeLayer.begin();
+	  TextBlock block2 = new TextBlock(plat.graphics().layoutText(
+					strikeCounterDisplay.toString(), 
+					new TextFormat(new Font("Helvetica", Font.Style.BOLD, 20)),
+					TextWrap.MANUAL));
+	  canvas2.clear();
+	  canvas2.setFillColor(0xFFFFFFFF);
+	  block2.fill(canvas2, TextBlock.Align.LEFT, 2, 2);
+	  rootLayer.addFloorAt(strikeLayer, 600, 20);
+	  strikeLayer.end();	
+  }
     
-	/*The main feature of the game. 
-	An interactive button that turns red when on, green when off, and blue when pressed
-	*/
+  /*The main feature of the game. 
+   An interactive button that turns red when on, green when off, and blue when pressed
+  */
   public class Button {
-        
 		Image[] images = {
 			plat.assets().getImage("images/red-button.png"),
 			plat.assets().getImage("images/inactive-button.png"),
@@ -136,31 +134,35 @@ public class PressTheRedButton extends SceneGame {
 		boolean isRed;
 		ImageLayer layer = new ImageLayer(images[0]);
 		
-		public Button(final GroupLayer buttonLayer, float x, float y, int color) {
-			this.layer.setOrigin(ImageLayer.Origin.CENTER);
-			buttonLayer.addAt(layer, x, y);
-			this.setColor(color);
-		}
+	public Button(final GroupLayer buttonLayer, float x, float y, int color) {
+	  this.layer.setOrigin(ImageLayer.Origin.CENTER);
+	  buttonLayer.addAt(layer, x, y);
+	  this.setColor(color);
+	}
+	
     public void setBlue(){
       this.layer.setSource(images[2]);
       this.buttonColor = 2;
       this.isRed = false;
     }
+    
     public void setRed(){
       this.layer.setSource(images[0]);
       this.isRed = true;
       buttonColor = 0;
     }
+    
     public void setInactive(){
       this.layer.setSource(images[1]);
       this.isRed = false;
       this.buttonColor = 1;
     }
+    
     public void setColor(int color){
       this.layer.setSource(images[color]);
-			if (color == 0){ this.isRed = true;}
-			else{this.isRed = false;}
-		}
+	  if (color == 0){ this.isRed = true;}
+	  else{this.isRed = false;}
+	}
   }
 
   public final Pointer pointer;
@@ -175,14 +177,14 @@ public class PressTheRedButton extends SceneGame {
     //plat.input().mouseEvents.connect(new Mouse.dispatcher(rootLayer, false)); 
     
 	
-	  //Initialize in the title state
-	  gameState = TITLE;
+	//Initialize in the title state
+	gameState = TITLE;
 	
-	  //Start on easiest difficulty
-	  gameCounterDifficulty = 0;
+	//Start on easiest difficulty
+	gameCounterDifficulty = 0;
 	
-	  //Background image for game
-	  Image bgImage = plat.assets().getImage("images/blackbg.jpg");
+	//Background image for game
+	Image bgImage = plat.assets().getImage("images/blackbg.jpg");
   	final ImageLayer bgLayer = new ImageLayer(bgImage);
 	
   	//Display for top-left score counter
@@ -201,8 +203,8 @@ public class PressTheRedButton extends SceneGame {
     final StringBuilder strikeCounterDisplay = new StringBuilder();
     strikeCounterDisplay.append("Strike Counter: \n" + strikeCounter);
     TextBlock block2 = new TextBlock(plat.graphics().layoutText(
-        strikeCounterDisplay.toString(),
-        new TextFormat(new Font("Helvetica", Font.Style.BOLD, 20)),
+    			strikeCounterDisplay.toString(),
+    			new TextFormat(new Font("Helvetica", Font.Style.BOLD, 20)),
 				TextWrap.MANUAL));
     final Canvas canvas2 = plat.graphics().createCanvas(block2.bounds.width()+16, block2.bounds.height()+8);
     canvas2.setFillColor(0xFFFFFFFF);
@@ -234,15 +236,15 @@ public class PressTheRedButton extends SceneGame {
     update.connect(new Slot<Clock>() {
       @Override public void onEmit (Clock clock) {
         if (gameState == PLAYING) {
-			    //Setting the gameCounterDifficulty higher makes the delay between the button changes shorter
-          if (score >= 2) {
+		//Setting the gameCounterDifficulty higher makes the delay between the button changes shorter
+        if (score >= 2) {
             gameCounterDifficulty = 1;
           }
-          if (score >= 5){
-              gameCounterDifficulty = 2;
+        if (score >= 5){
+            gameCounterDifficulty = 2;
           }
-          if (score >= 7){
-              gameCounterDifficulty = 3;
+        if (score >= 7){
+            gameCounterDifficulty = 3;
           }
         
 			    //The gameCounter is set equal to the gameCounterDifficulty after calculating the player's score
@@ -280,106 +282,106 @@ public class PressTheRedButton extends SceneGame {
       }
     });
       
-       pointer = new Pointer(plat, rootLayer, false);
-       plat.input().mouseEvents.connect(new Mouse.Dispatcher(rootLayer, false));
+    pointer = new Pointer(plat, rootLayer, false);
+    plat.input().mouseEvents.connect(new Mouse.Dispatcher(rootLayer, false));
 
-         pointer.events.connect(new Slot<Pointer.Event>() {
-         @Override public void onEmit (Pointer.Event event) {
-                 if (gameState == TITLE){
-                        buttonPressed = true;
-						gameState = PLAYING;
-						populateScreen(bgLayer, buttonLayer);
-						updateScore(scoreDisplay, scoreBoardLayer, canvas);
-                        updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
-					}
-                if(gameState == PLAYING && buttonOne.isRed == true){
-						buttonOne.setBlue();
-                        buttonPressed = true;
-                        score++;
-                        updateScore(scoreDisplay, scoreBoardLayer, canvas);
-					}
-                if (gameState == PLAYING && buttonTwo.isRed){
-                    buttonTwo.setBlue();
-                    buttonPressed = true;
-                    score++;
-                    updateScore(scoreDisplay, scoreBoardLayer, canvas);
-                }
-                if (gameState == PLAYING && buttonThree.isRed == true){
-                        buttonThree.setBlue();
-						score++;
-                        buttonPressed = true;
-                        updateScore(scoreDisplay, scoreBoardLayer, canvas);
-					}
+    pointer.events.connect(new Slot<Pointer.Event>() {
+    	@Override public void onEmit (Pointer.Event event) {
+            if (gameState == TITLE){
+                buttonPressed = true;
+				gameState = PLAYING;
+				populateScreen(bgLayer, buttonLayer);
+				updateScore(scoreDisplay, scoreBoardLayer, canvas);
+                updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
+			}
+            if(gameState == PLAYING && buttonOne.isRed == true){
+				buttonOne.setBlue();
+                buttonPressed = true;
+                score++;
+                updateScore(scoreDisplay, scoreBoardLayer, canvas);
+			}
+            if (gameState == PLAYING && buttonTwo.isRed){
+                buttonTwo.setBlue();
+                buttonPressed = true;
+                score++;
+                updateScore(scoreDisplay, scoreBoardLayer, canvas);
+            }
+            if (gameState == PLAYING && buttonThree.isRed == true){
+                buttonThree.setBlue();
+				score++;
+                buttonPressed = true;
+                updateScore(scoreDisplay, scoreBoardLayer, canvas);
+			}
         }
       });
         
-		// when the keyboard button is pressed, do a thing
-		plat.input().keyboardEvents.connect(new Keyboard.KeySlot(){
-		@Override public void onEmit (Keyboard.KeyEvent event) {
-			switch (event.key){
+	// when the keyboard button is pressed, do a thing
+	plat.input().keyboardEvents.connect(new Keyboard.KeySlot(){
+	@Override public void onEmit (Keyboard.KeyEvent event) {
+		switch (event.key){
 			// Case for down keyboard arrow key
-				case DOWN:
-					if (gameState == TITLE && event.down){
-            buttonPressed = true;
-						gameState = PLAYING;
-						populateScreen(bgLayer, buttonLayer);
-						updateScore(scoreDisplay, scoreBoardLayer, canvas);
-            updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
-						break;
-					} 
-          if (gameState == GAMEOVER && event.down){
-            gameState = PLAYING;
-            populateScreen(bgLayer, buttonLayer);
-            updateScore(scoreDisplay, scoreBoardLayer, canvas);
-						updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
-            break;
+			case DOWN:
+				if (gameState == TITLE && event.down){
+					buttonPressed = true;
+					gameState = PLAYING;
+					populateScreen(bgLayer, buttonLayer);
+					updateScore(scoreDisplay, scoreBoardLayer, canvas);
+					updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
+					break;
+				} 
+				if (gameState == GAMEOVER && event.down){
+					gameState = PLAYING;
+					populateScreen(bgLayer, buttonLayer);
+					updateScore(scoreDisplay, scoreBoardLayer, canvas);
+					updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
+					break;
+				}
+				if (gameState == PLAYING && event.down && buttonTwo.isRed == true){
+					score++;
+					buttonPressed = true;
+					updateScore(scoreDisplay, scoreBoardLayer, canvas);
+					buttonTwo.setBlue();
+					break;
+				}
+				if (gameState == PLAYING && event.down && buttonTwo.isRed == false){
+					gameState = GAMEOVER;
+					populateScreen(bgLayer, titleLayer);
+					updateScore(scoreDisplay, scoreBoardLayer, canvas);
+					updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
+					break;
           }
-					if (gameState == PLAYING && event.down && buttonTwo.isRed == true){
-            score++;
-            buttonPressed = true;
-            updateScore(scoreDisplay, scoreBoardLayer, canvas);
-						buttonTwo.setBlue();
-						break;
-          }
-					if (gameState == PLAYING && event.down && buttonTwo.isRed == false){
-						gameState = GAMEOVER;
-						populateScreen(bgLayer, titleLayer);
-            updateScore(scoreDisplay, scoreBoardLayer, canvas);
-            updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
-						break;
-          }
-				// Case for Left keyboard arrow key
-        case LEFT:
-					if(gameState == PLAYING && event.down && buttonOne.isRed == true){
-						buttonOne.setBlue();
-            buttonPressed = true;
-            score++;
-            updateScore(scoreDisplay, scoreBoardLayer, canvas);
-						break;
-					}
-					if (gameState == PLAYING && event.down && buttonOne.isRed == false){
-						gameState = GAMEOVER;
-						populateScreen(bgLayer, titleLayer);
-            updateScore(scoreDisplay, scoreBoardLayer, canvas);
-            updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
-						break;
-					}	
-				// Case for right keyboard arrow key
-				case RIGHT:
-					if (gameState == PLAYING && event.down && buttonThree.isRed == true){
-            buttonThree.setBlue();
-						score++;
-            buttonPressed = true;
-            updateScore(scoreDisplay, scoreBoardLayer, canvas);
-						break;
-					}
-					if (gameState == PLAYING && event.down && buttonThree.isRed == false){
-						gameState = GAMEOVER;
-						populateScreen(bgLayer, titleLayer);
-            updateScore(scoreDisplay, scoreBoardLayer, canvas);
-            updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
-						break;
-					}
+			// Case for Left keyboard arrow key
+			case LEFT:
+				if(gameState == PLAYING && event.down && buttonOne.isRed == true){
+					buttonOne.setBlue();
+					buttonPressed = true;
+					score++;
+					updateScore(scoreDisplay, scoreBoardLayer, canvas);
+					break;
+				}
+				if (gameState == PLAYING && event.down && buttonOne.isRed == false){
+					gameState = GAMEOVER;
+					populateScreen(bgLayer, titleLayer);
+					updateScore(scoreDisplay, scoreBoardLayer, canvas);
+					updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
+					break;
+				}	
+			// Case for right keyboard arrow key
+			case RIGHT:
+				if (gameState == PLAYING && event.down && buttonThree.isRed == true){
+					buttonThree.setBlue();
+					score++;
+					buttonPressed = true;
+					updateScore(scoreDisplay, scoreBoardLayer, canvas);
+					break;
+				}
+				if (gameState == PLAYING && event.down && buttonThree.isRed == false){
+					gameState = GAMEOVER;
+					populateScreen(bgLayer, titleLayer);
+					updateScore(scoreDisplay, scoreBoardLayer, canvas);
+					updateStrikeCounter(strikeCounterDisplay, strikeLayer, canvas2);
+					break;
+				}
 				default:
 					break;
 			} 
